656b90c92448c38818eb8c6e41a62cb7e67d6e76
Mfundo_debug <diditmfundo@gmail.com>
2023-07-25 04:13:54 +0200

    NO IMPROVEMENTS

--- feature.js
+++ feature.js
@@ -1,21 +1,42 @@
-// Get the elements
-const cardNumberElement = document.querySelector('.card-number');
-const nameElement = document.querySelector('.name');
-const validDateElement = document.querySelector('.valid-date');
+document.addEventListener("DOMContentLoaded", function() {
+  const card = document.querySelector(".card");
+  const cardFront = document.querySelector(".front-face");
+  const cardBack = document.querySelector(".back-face");
+  const formSection = document.querySelector(".fill-in-form");
 
-// Function to update card details in real-time
-function updateCardDetails() {
-  cardNumberElement.textContent = cardNumberInput.value;
-  nameElement.textContent = nameInput.value;
-  validDateElement.textContent = validDateInput.value;
-}
+  // Flip the card on click
+  card.addEventListener("click", function() {
+    card.classList.toggle("flipped");
+  });
 
-// Add event listeners to input fields
-const cardNumberInput = document.getElementById('card-number');
-cardNumberInput.addEventListener('input', updateCardDetails);
+  // Show the fill-in form when the card is flipped
+  card.addEventListener("transitionend", function() {
+    if (card.classList.contains("flipped")) {
+      formSection.style.display = "block";
+    } else {
+      formSection.style.display = "none";
+    }
+  });
 
-const nameInput = document.getElementById('name');
-nameInput.addEventListener('input', updateCardDetails);
+  // Validate and format the card number input
+  const cardNumberInput = document.getElementById("card-number");
+  cardNumberInput.addEventListener("input", function() {
+    let trimmedValue = this.value.replace(/\s+/g, ""); // Remove spaces
+    let formattedValue = "";
+    for (let i = 0; i < trimmedValue.length; i += 4) {
+      formattedValue += trimmedValue.substr(i, 4) + " ";
+    }
+    this.value = formattedValue.trim();
+  });
 
-const validDateInput = document.getElementById('valid-date');
-validDateInput.addEventListener('input', updateCardDetails);
+  // Validate and format the valid date input
+  const validDateInput = document.getElementById("valid-date");
+  validDateInput.addEventListener("input", function() {
+    let trimmedValue = this.value.replace(/\s+/g, ""); // Remove spaces
+    let formattedValue = "";
+    for (let i = 0; i < trimmedValue.length; i += 2) {
+      formattedValue += trimmedValue.substr(i, 2) + "/";
+    }
+    this.value = formattedValue.trim();
+  });
+});
